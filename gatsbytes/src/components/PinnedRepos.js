84236b3898431813939c08d149e1b6c1f089efbf
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2023-07-31 10:30:16 +0200

    finish styles

--- gatsbytes/src/components/PinnedRepos.js
+++ gatsbytes/src/components/PinnedRepos.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as styles from './pinnedrepos.module.css';
+import { AiOutlineStar, AiOutlineFork, AiFillBook } from 'react-icons/ai';
 
+const languageColors = {
+    JavaScript: "#f1e05a",
+    Python: "#3572A5",
+    "Jupyter Notebook": "#DA5B0B",
+    HCL: "#844FBA",
+    // Add more language-color pairs
+};
+
+const truncateDescription = (description, maxLength) => {
+    if (!description) return '';
+    if (description.length <= maxLength) {
+        return description;
+    }
+
+    return `${description.substring(0, maxLength)}...`;
+}
 
 const PinnedRepos = () => {
     const [repos, setRepos] = useState([]);
@@ -23,6 +40,9 @@ const PinnedRepos = () => {
                 description
                 stargazerCount
                 forkCount
+                primaryLanguage {
+                    name
+                }
               }
             }
           }
@@ -58,17 +78,22 @@ const PinnedRepos = () => {
             {repos && repos.length > 0 && repos.map((repo) => (
                 <div key={repo.id} className={styles.repoCard}>
                     <a href={repo.url} target="_blank" rel="noopener noreferrer">
-                        {repo.name}
+                        <AiFillBook className={styles.icon}/> {repo.name}
                     </a>
-                    <p>{repo.description}</p>
-                    <p>Stars: {repo.stargazerCount} | Forks: {repo.forkCount}</p>
+                    <p className={styles.repoDescription}>{truncateDescription(repo.description, 100)}</p>
+                    <div className={styles.repoStats}>
+                        <span 
+                            className={styles.languageBubble} 
+                            style={{ backgroundColor: languageColors[repo.primaryLanguage.name] }}
+                        ></span>
+                        {repo.primaryLanguage.name} 
+                        <AiOutlineStar className={styles.icon}/> {repo.stargazerCount} 
+                        <AiOutlineFork className={styles.icon}/> {repo.forkCount}
+                    </div>
                 </div>
             ))}
         </div>
     );
 }
-
-console.log('Github Token:', process.env.GATSBY_GITHUB_TOKEN);
-
 
 export default PinnedRepos;
